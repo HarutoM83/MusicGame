6caa055c185ad092e52876e4257aace485a8e63e
HarutoM83 <ktc24a32a0010@edu.kyoto-tech.ac.jp>
2026-07-04 16:21:48 +0900

    ノーツデザイン?とノーツマテリアルを変更

---
+++ Assets/Scripts/ObjectPool_Notes.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.Pool;
+public class ObjectPool_Notes : MonoBehaviour
+{
+    ObjectPool<GameObject> pool;    // オブジェクトプールの変数を宣言
+    public GameObject obj;  // プールの中で管理したいオブジェクト
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // オブジェクトプールのインスタンスを生成
+        pool = new ObjectPool<GameObject>(
+            CreatePooledItem,       // オブジェクト生成の際の処理
+            OnTakeFromPool,         // オブジェクトを取り出す際の処理
+            OnReturnedToPool,       // オブジェクトを返却する際の処理
+            OnDestroyPoolObject,    // プールが上限を超えた場合の処理
+            true,                   // すでにプール内にいるオブジェクトを返却した際にエラー表示するか
+            2,                      // 初期のプールの容量
+            10);                    // プール内オブジェクトの上限数
+    }
+
+    // オブジェクト生成の際の処理
+    GameObject CreatePooledItem()
+    {
+        return Instantiate(obj);    // オブジェクトを生成してプールに渡す処理
+    }
+
+    // オブジェクトを取り出す際の処理
+    void OnTakeFromPool(GameObject obj)
+    {
+        obj.SetActive(true);    // オブジェクトをアクティブにする処理
+        obj.transform.position = new Vector2(Random.Range(-8f, 8f), Random.Range(-4.5f, 4.5f));  // オブジェクトの座標を指定する処理
+    }
+
+    // オブジェクトを返却する際の処理
+    void OnReturnedToPool(GameObject obj)
+    {
+        obj.SetActive(false);   // オブジェクトを非アクティブにする処理
+    }
+
+    // プールが上限を超えた場合の処理
+    void OnDestroyPoolObject(GameObject obj)
+    {
+        Destroy(obj);    // オブジェクトを破壊する処理
+    }
+
+    // 他のクラスからオブジェクトを取り出せるようにするためのメソッド
+    public GameObject GetObject()
+    {
+        return pool.Get();  // プールからオブジェクトを取り出す処理
+    }
+
+    // 他のクラスからオブジェクトを返却できるようにするためのメソッド
+    public void ReleaseObject(GameObject obj)
+    {
+        pool.Release(obj);  // プールにオブジェクトを返却する処理
+    }
+}
